09a671d706c9244adafb6abf65ae7899004d86be
Kai Avni <kaiavni10@gmail.com>
2023-03-24 01:27:21 -0400

    implemented Ingredient model class and recyclerView

--- app/src/main/java/com/tganesh/pantrypal/MainActivity.java
+++ app/src/main/java/com/tganesh/pantrypal/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inflate the PantryFragment
+        PantryFragment pantryFragment = new PantryFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.activity_layout, pantryFragment, "PantryFragment")
+                .commit();
     }
 }
